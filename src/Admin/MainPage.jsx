873914b7cca545e1c1f62e57feb5bd0493d8a41c
krakenlink243 <vquochoang7@gmail.com>
2025-03-21 01:27:36 +0700

    update

--- src/Admin/MainPage.jsx
+++ src/Admin/MainPage.jsx
@@ -1,13 +1,36 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import Header from './Header'
 import NavPane from './NavPane'
 import './MainPage.css'
+=======
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import NavPane from './NavPane';
+import './MainPage.css';
+import Login from './Login';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import AddPost from './AddPost';
+import PostManager from './PostManager';
+>>>>>>> Stashed changes
 function MainPage() {
   return (
     <div>
+<<<<<<< Updated upstream
       <div className='fixed-top'>
         <NavPane/>
         <Header/>
+=======
+        <div className='fixed-top'>
+          <NavPane />
+          <div className='right-side'>
+            <Header />
+            <div className='inside-right-side'>
+              <PostManager />
+            </div>
+          </div>
+>>>>>>> Stashed changes
         </div>
     </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import Maincard from './fuctnews/maincard'
 import Contentcard from './fuctnews/contentcard'
@@ -11,8 +12,31 @@ import Tips from './fuctKnowledge/Tips'
 import Other from './fuctKnowledge/Other'
 import KnowledgeMenu from './fuctKnowledge/KnowledgeMenu'
 import MainPage from './Admin/MainPage'
+=======
+import Header from './Header';
+import Footer from './Footer';
+import './styles/index.css';
+import MainTestScreen from './MainTestScreen';
+import MainPageAdmin from './Admin/MainPage';
+import TestIntroScreen from './components/TestIntroScreen';
+import GenderScreen from './components/GenderScreen';
+import HeightWeightScreen from './components/HeightWeightScreen';
+import BodyShapeScreen from './components/BodyShapeScreen';
+import { Route, Routes } from 'react-router-dom';
+import UndertoneScreen from './components/UndertoneScreen';
+import ResultScreen from './components/ResultScreen';
+import LegComputeScreen from './components/LegComputeScreen';
+import News from './fuctnews/News.jsx'
+import KnowledgeMenu from './fuctKnowledge/KnowledgeMenu';
+import MainPage from './MainPage';
+import AddPost from './Admin/AddPost.jsx';
+import { useState } from 'react';
+import Login from './Admin/Login.jsx';
+import NewsContent from './fuctnews/NewsContent.jsx';
+>>>>>>> Stashed changes
 function App() {
   return (
+<<<<<<< Updated upstream
     <>
       {/* <News/> */}
       {/* <NewsContent/> */}
@@ -26,6 +50,40 @@ function App() {
 
     </>
   )
+=======
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      fontFamily:'serif',  
+    }}>
+    
+      <Header />  
+      <Routes>
+        <Route path='/' element={<MainPage />}w />
+        <Route path='/news' element={<News/>}/>
+        <Route path='/news/content' element={<NewsContent/>}/>
+        <Route path='/knowledge' element={<KnowledgeMenu/>}/>
+        <Route path='/admin/success' element={<MainPageAdmin/>} />
+        <Route path={isLoggedIn? '/admin/success': '/admin'} element={isLoggedIn ? <MainPageAdmin /> : <Login onLogin={() => setIsLoggedIn(true)} />} />
+        {/* <Route path='/admin' element={<Login/>} /> */}
+        <Route path='/admin/addpost' element={<AddPost />} />
+        <Route path='/test' element={<MainTestScreen />}>
+          <Route index element={<TestIntroScreen/>}/>
+          <Route path='/test/intro' element={<TestIntroScreen />} />
+          <Route path='/test/gender' element={<GenderScreen />} />
+          <Route path='/test/height-weight' element={<HeightWeightScreen />} />
+          <Route path='/test/body-shape' element={<BodyShapeScreen />} />
+          <Route path='/test/undertone' element={<UndertoneScreen/>}/>
+          <Route path='/test/result' element={<ResultScreen/>}/>
+          <Route path='/test/leglength' element={<LegComputeScreen/>}/>
+          
+        </Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
