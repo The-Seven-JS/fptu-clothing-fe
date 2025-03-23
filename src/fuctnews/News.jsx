57866180d879c07e573b938ea42a21fa519c08c4
krakenlink243 <vquochoang7@gmail.com>
2025-03-24 02:14:50 +0700

    fix api

--- src/fuctnews/News.jsx
+++ src/fuctnews/News.jsx
@@ -10,7 +10,7 @@ function News() {
     const [data, setData] = useState([]);//fetch data tu db
     const [content, setContent] = useState([]);//fetch data tu db
     useEffect(() => {
-      axios.get("http://34.87.162.201:3000/articles").then((res) => {
+      axios.get("https://be.fuct.gay/articles").then((res) => {
         setData(res.data.slice(res.data.length-5,res.data.length).reverse());
         setContent(res.data.slice().reverse());
       });
