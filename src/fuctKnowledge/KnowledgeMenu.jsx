79d2692f7573efec31b089b12afbb2aa1e36dda6
krakenlink243 <vquochoang7@gmail.com>
2025-03-24 12:35:10 +0700

    add image to knowledge

--- src/fuctKnowledge/KnowledgeMenu.jsx
+++ src/fuctKnowledge/KnowledgeMenu.jsx
@@ -4,19 +4,19 @@ function KnowledgeMenu() {
   return (
     <div className="background">
         <div className='item'>
-            <img src="https://s3-alpha-sig.figma.com/img/65d6/8f72/4f70b8f021d5f479cd5eb6004696c267?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k~GPpBnsawDc5D8u1OziHx1J6j~IJES6DrI-xypsMJmpyiBtqzhrApJKkTZu2yufOVvjHb6IPKYfSrV5aSEb7j9ViqtSCWkeGSEYZkLjsA3dAe7GuBMTtqkF8qjLyOLE-xCQVS0KL3s9t-b57vn7-NFO-KkcSaAicKPqpf8myUl1gZTAiaG-Kakc3r7u~weUooXLUnk7KnszO-93FyMcG-RiDQrB423Tjlmg5t8bQ7Vy602XftWU1X-Rg2a74L1Fu-BP0709FKELff5cssECTspg-pA3yWwzgmHQpxnxJFnZdQ7DGoebh6P5MB-qOAyBySFyesrX4f2zY4hKogXRrw__" alt="" />
+            <img src="/image/fuctKnowledge/ngoaihinh.jpeg" alt="" />
             <p>NGOẠI HÌNH</p>
         </div>
         <div className='item'>
-            <img src="https://s3-alpha-sig.figma.com/img/65d6/8f72/4f70b8f021d5f479cd5eb6004696c267?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k~GPpBnsawDc5D8u1OziHx1J6j~IJES6DrI-xypsMJmpyiBtqzhrApJKkTZu2yufOVvjHb6IPKYfSrV5aSEb7j9ViqtSCWkeGSEYZkLjsA3dAe7GuBMTtqkF8qjLyOLE-xCQVS0KL3s9t-b57vn7-NFO-KkcSaAicKPqpf8myUl1gZTAiaG-Kakc3r7u~weUooXLUnk7KnszO-93FyMcG-RiDQrB423Tjlmg5t8bQ7Vy602XftWU1X-Rg2a74L1Fu-BP0709FKELff5cssECTspg-pA3yWwzgmHQpxnxJFnZdQ7DGoebh6P5MB-qOAyBySFyesrX4f2zY4hKogXRrw__" alt="" />
+            <img src="/image/fuctKnowledge/phukien.jpg" alt="" />
             <p>PHỤ KIỆN</p>
         </div>
         <div className='item'>
-            <img src="https://s3-alpha-sig.figma.com/img/65d6/8f72/4f70b8f021d5f479cd5eb6004696c267?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k~GPpBnsawDc5D8u1OziHx1J6j~IJES6DrI-xypsMJmpyiBtqzhrApJKkTZu2yufOVvjHb6IPKYfSrV5aSEb7j9ViqtSCWkeGSEYZkLjsA3dAe7GuBMTtqkF8qjLyOLE-xCQVS0KL3s9t-b57vn7-NFO-KkcSaAicKPqpf8myUl1gZTAiaG-Kakc3r7u~weUooXLUnk7KnszO-93FyMcG-RiDQrB423Tjlmg5t8bQ7Vy602XftWU1X-Rg2a74L1Fu-BP0709FKELff5cssECTspg-pA3yWwzgmHQpxnxJFnZdQ7DGoebh6P5MB-qOAyBySFyesrX4f2zY4hKogXRrw__" alt="" />
+            <img src="/image/fuctKnowledge/phoido.jpg" alt="" />
             <p>PHỐI ĐỒ</p>
         </div>
         <div className='item'>
-            <img src="https://s3-alpha-sig.figma.com/img/65d6/8f72/4f70b8f021d5f479cd5eb6004696c267?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k~GPpBnsawDc5D8u1OziHx1J6j~IJES6DrI-xypsMJmpyiBtqzhrApJKkTZu2yufOVvjHb6IPKYfSrV5aSEb7j9ViqtSCWkeGSEYZkLjsA3dAe7GuBMTtqkF8qjLyOLE-xCQVS0KL3s9t-b57vn7-NFO-KkcSaAicKPqpf8myUl1gZTAiaG-Kakc3r7u~weUooXLUnk7KnszO-93FyMcG-RiDQrB423Tjlmg5t8bQ7Vy602XftWU1X-Rg2a74L1Fu-BP0709FKELff5cssECTspg-pA3yWwzgmHQpxnxJFnZdQ7DGoebh6P5MB-qOAyBySFyesrX4f2zY4hKogXRrw__" alt="" />
+            <img src="/image/fuctKnowledge/khac.jpg" alt="" />
             <p>KHÁC</p>
         </div>
             
